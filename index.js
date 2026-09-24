7ab7cd9f0f45d7db22a6de2908bac1d4fa58f050
DanielMontegomeri <danielmontegomery.barros@gmail.com>
2026-09-24 10:49:40 -0400

    projeto finalizado

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// eu não aguento mais
-console.log("n to aguentando mais")
+let nomeDoHeroi = ""
+let pontos = 1
+let divisoes = [ 
+     0, 1000 ,
+     2000, 5000, 
+     7000, 8000,
+     9000, 10000]
+let patentes = [
+    "Ferro", "Bronze",
+    "Prata", "Ouro",
+    "Platina", "Ascendente",
+    "Imortal", "Radiante"
+]
+
+
+for(let i = patentes[0]; i < divisoes[]; i++  ){
+    let rankCalculator = divisoes[0]
+        if(pontos > divisoes){
+            rankCalculator++
+
+}
+
+}
